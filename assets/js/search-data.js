7e453426162dd85c66670a783c49e8268b4034da
ying-fu <47652356+ying-fu@users.noreply.github.com>
2025-01-17 12:22:54 +0000

    Deploying to gh-pages from @ ying-fu/ying-fu.github.io@fbee860d826fc5f8f935482527fc4de0e01d6b1b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,6 +16,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
+        },{id: "nav-openings",
+          title: "Openings",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/openings/";
+          },
         },{id: "post-a-post-with-image-galleries",
       
         title: "a post with image galleries",
